9e8657eec2c13a22bcdf9320939faa17d70c3b23
trankilidat <aahammoud@outlook.com>
2025-04-08 21:58:06 +0200

    upload thumbnail exercise'

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,64 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	fmt.Println("parsed multi part form")
+
+	// "thumbnail" should match the HTML form input name
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType := header.Header.Get("Content-Type")
+
+	fmt.Println("mediatype", mediaType)
+
+	// read image into byte slice
+	imgSlice, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to read image data into byte slice", err)
+		return
+	}
+
+	// get video metadata
+	vm, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to get video", err)
+		return
+	}
+
+	if vm.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "User is not authorized to access video", err)
+		return
+	}
+
+	// add to global map
+	tn := thumbnail{
+		data:      imgSlice,
+		mediaType: mediaType,
+	}
+
+	videoThumbnails[vm.ID] = tn
+
+	// add thumnail url to video metadata
+	tnu := fmt.Sprintf(`http://localhost:8091/api/thumbnails/{%s}`, vm.ID.String())
+	vm.ThumbnailURL = &tnu
+
+	fmt.Printf("\n video %+v", vm)
+
+	// update video with new thumbnail url
+	if err := cfg.db.UpdateVideo(vm); err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Unable to update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, vm)
 }
